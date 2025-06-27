3ff7f8dc31f32a4a5c29a7cce7858f8bd7b5d003
Jayasurya <suryaselva0727@gmail.com>
2025-06-27 16:36:00 +0530

    Update sql dmart sales analysis.sql

--- sql dmart sales analysis.sql
+++ sql dmart sales analysis.sql
@@ -1,211 +1,138 @@
-CREATE DATABASE IF NOT EXISTS DmartSales ;
+-- DATABASE CREATION
+CREATE DATABASE IF NOT EXISTS DmartSales;
 
+-- SELECT THE DATABASE
+USE DmartSales;
 
-CREATE TABLE IF NOT EXISTS sales(
-invoice_id VARCHAR(20) NOT NULL PRIMARY KEY,
-branch VARCHAR(10) NOT NULL,
-city VARCHAR(30) NOT NULL,
-customer_type VARCHAR(30) NOT NULL,
-gender VARCHAR(10) NOT NULL,
-product_line VARCHAR(50) NOT NULL,
-unit_price DECIMAL(10,3) NOT NULL,
-quantity INT NOT NULL,
-VAT FLOAT(6,4),
-total DECIMAL(12,4) NOT NULL,
-date DATETIME NOT NULL,
-time TIME NOT NULL,
-payment_method VARCHAR(10),
-cogs DECIMAL(10,2),
-gross_margin_pct FLOAT(11,9),
-gross_income DECIMAL(12,4),
-rating FLOAT(2,1)
+-- TABLE CREATION
+CREATE TABLE IF NOT EXISTS sales (
+    invoice_id VARCHAR(20) NOT NULL PRIMARY KEY,
+    branch VARCHAR(10) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    customer_type VARCHAR(30) NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    product_line VARCHAR(50) NOT NULL,
+    unit_price DECIMAL(10,3) NOT NULL,
+    quantity INT NOT NULL,
+    VAT FLOAT(6,4),
+    total DECIMAL(12,4) NOT NULL,
+    date DATETIME NOT NULL,
+    time TIME NOT NULL,
+    payment_method VARCHAR(20),
+    cogs DECIMAL(10,2),
+    gross_margin_pct FLOAT(11,9),
+    gross_income DECIMAL(12,4),
+    rating FLOAT(2,1)
 );
 
--- ---------------------------------------------------------------
--- ---------------FEATURE ENGINEERING-----------------------------
+-- ----------------------------------------------------------------
+-- FEATURE ENGINEERING
+-- ----------------------------------------------------------------
 
--- TIME OF SALE --
-
-
-SELECT 
-     time,
-     (CASE
-          WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "morning"
-		  WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "afternoon"
-          ELSE "evening"     
-      END
-     ) AS time_of_sale
-FROM sales;
- 
-ALTER TABLE sales add column time_of_sale VARCHAR(20);
+-- TIME OF SALE
+ALTER TABLE sales ADD COLUMN time_of_sale VARCHAR(20);
 
 UPDATE sales 
-      SET time_of_sale = 
-      (
-      CASE
-          WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "morning"
-		  WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "afternoon"
-          ELSE "evening"     
-      END
-      );
-      
--- ------------------------------------------------------------------
--- ------------------day_name---------------------------------------
+SET time_of_sale = CASE
+    WHEN `time` BETWEEN '00:00:00' AND '12:00:00' THEN 'morning'
+    WHEN `time` BETWEEN '12:01:00' AND '16:00:00' THEN 'afternoon'
+    ELSE 'evening'
+END;
 
-SELECT 
-	DATE,DAYNAME(`date`) 
-FROM sales
+-- DAY NAME
+ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);
+UPDATE sales SET day_name = DAYNAME(`date`);
 
-ALTER TABLE sales 
-ADD COLUMN day_name VARCHAR(10);
-
-UPDATE sales 
-SET day_name = DAYNAME(`date`) 
+-- MONTH NAME
+ALTER TABLE sales ADD COLUMN month_name VARCHAR(10);
+UPDATE sales SET month_name = MONTHNAME(`date`);
 
 -- ----------------------------------------------------------------
--- ------------------MONTH_NAME------------------------------------
+-- ANALYSIS QUERIES
+-- ----------------------------------------------------------------
 
-SELECT 
-    DATE,MONTHNAME(DATE)
-FROM SALES;
+-- 1. Unique Cities
+SELECT DISTINCT city FROM sales;
 
-ALTER TABLE sales 
-ADD COLUMN MONTH_NAME VARCHAR(10)
+-- 2. Branches in Each City
+SELECT DISTINCT branch, city FROM sales;
 
-UPDATE sales 
-set month_name = MONTHNAME(`DATE`)
+-- 3. Unique Product Lines
+SELECT COUNT(DISTINCT product_line) AS unique_product_lines FROM sales;
 
--- -------------------------------------------------------------------
--- -------------------"GENERIC"---------------------------------------
-
-
--- HOW MANY UNIQUE CITIES DOES THE DATA HAVE ?
-
-
-SELECT 
-     DISTINCT city 
-FROM sales;
-
--- IN WHICH CITY IN EACH BRANCH
-
-
-
--- ----------------------------PRODUCT----------------------------------------
-
--- How many unique product lines does the data have?
-
-SELECT
-     COUNT(DISTINCT product_line) 
-FROM sales;
-\
--- What is the most common payment method?
-
-SELECT 
-     payment_method , count(payment_method) AS count
+-- 4. Most Common Payment Method
+SELECT payment_method, COUNT(*) AS count
 FROM sales
 GROUP BY payment_method
-ORDER BY COUNT DESC limit 1
+ORDER BY count DESC
+LIMIT 1;
 
--- What is the most selling product line?
-
-SELECT 
-     product_line, count(product_line) AS count
+-- 5. Most Selling Product Line
+SELECT product_line, COUNT(*) AS count
 FROM sales
 GROUP BY product_line
-ORDER BY COUNT DESC limit 1
+ORDER BY count DESC
+LIMIT 1;
 
--- What is the total revenue by month?
-
-SELECT 
-     month_name,sum(total) as total 
-FROM sales 
-GROUP BY month_name 
-ORDER BY total DESC
-
--- Which month had the largest COGS?
-
-SELECT 
-     month_name AS month , sum(cogs) 
-FROM SALES 
+-- 6. Total Revenue by Month
+SELECT month_name, SUM(total) AS total_revenue
+FROM sales
 GROUP BY month_name
-ORDER BY SUM(cogs) DESC
+ORDER BY total_revenue DESC;
 
--- What product line had the largest revenue?
+-- 7. Month with Highest COGS
+SELECT month_name AS month, SUM(cogs) AS total_cogs
+FROM sales
+GROUP BY month_name
+ORDER BY total_cogs DESC
+LIMIT 1;
 
+-- 8. Product Line with Highest Revenue
+SELECT product_line, SUM(total) AS revenue
+FROM sales
+GROUP BY product_line
+ORDER BY revenue DESC
+LIMIT 1;
 
+-- 9. City with Highest Revenue
+SELECT city, SUM(total) AS revenue
+FROM sales
+GROUP BY city
+ORDER BY revenue DESC
+LIMIT 1;
 
--- What is the city with the largest revenue?
-
-SELECT 
-    city,SUM(TOTAL) AS revenue 
-FROM sales 
-GROUP BY city 
-ORDER BY revenue DESC 
-
--- What product line had the largest VAT?
-
-SELECT 
-     product_line,SUM(vat) AS total_vat
+-- 10. Product Line with Highest VAT
+SELECT product_line, SUM(VAT) AS total_vat
 FROM sales
 GROUP BY product_line
 ORDER BY total_vat DESC
+LIMIT 1;
 
--- Fetch each product line and add a column to those product line showing "Good", "Bad". Good if its greater than average sales
-
+-- 11. Tag Product Lines as "Good" or "Bad" based on Average Quantity
 SELECT 
-     AVG(quantity) AS avg_qnty  
-FROM sales 
+    product_line,
+    AVG(quantity) AS avg_quantity,
+    CASE 
+        WHEN AVG(quantity) > (SELECT AVG(quantity) FROM sales) THEN 'Good'
+        ELSE 'Bad'
+    END AS sales_performance
+FROM sales
+GROUP BY product_line;
 
-SELECT 
-     product_line,(
-     CASE
-     WHEN AVG(quantity)<5.5 THEN "good"
-     ELSE "bad"
-     END
-     ) AS avg_sales   
+-- 12. Branches That Sold More Than Average Quantity
+SELECT branch, SUM(quantity) AS total_quantity
+FROM sales
+GROUP BY branch
+HAVING total_quantity > (SELECT AVG(quantity) FROM sales);
+
+-- 13. Most Common Product Line by Gender
+SELECT gender, product_line, COUNT(*) AS count
+FROM sales
+GROUP BY gender, product_line
+ORDER BY count DESC;
+
+-- 14. Average Rating of Each Product Line
+SELECT product_line, ROUND(AVG(rating), 2) AS avg_rating
 FROM sales
 GROUP BY product_line
-
--- Which branch sold more products than average product sold?
-
-SELECT 
-     branch,SUM(quantity) as qnty
-FROM sales 
-GROUP BY branch 
-HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);
-
--- What is the most common product line by gender?
-
-SELECT 
-    product_line, gender ,count(gender) 
-FROM sales 
-GROUP BY gender,product_line 
-ORDER BY count(gender) DESC
-
--- What is the average rating of each product line?
-
-SELECT product_line,ROUND(avg(rating),2) AS avg_rating FROM sales GROUP BY product_line ORDER BY avg_rating desc
-
-
-
-     
-     
-     
-     
-
-
-
-
-     
-
-
-
-
-
-        
-     
-
-
-
-     
-
+ORDER BY avg_rating DESC;
